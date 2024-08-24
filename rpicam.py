070f5edf481684ff4ee1e54cc8404786afdcb727
DEYOU NGOUNOU Cyril Landry <53407853+cyrildey@users.noreply.github.com>
2024-08-24 17:42:53 +0200

    Time delay added

--- rpicam.py
+++ rpicam.py
@@ -1,9 +1,16 @@
 import os
 import main
-# Capture video from webcam
-image_number = 0
-image_path = "./captures/" + str(image_number) + ".jpg"
-os.system("rpicam-still -o " + image_path)
-image_number += 1
-# Call the prediction function
-main.single_prediction(image_path)
+import time
+
+def predict():
+    # Capture video from webcam
+    image_number = 0
+    image_path = "./captures/" + str(image_number) + ".jpg"
+    os.system("rpicam-still -o " + image_path)
+    image_number += 1
+    # Call the prediction function
+    main.single_prediction(image_path)
+
+while True:
+    predict()
+    time.sleep(1)
