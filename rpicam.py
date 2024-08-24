71fa8020b697e761abe4a467883f315e1a27f262
DEYOU NGOUNOU Cyril Landry <53407853+cyrildey@users.noreply.github.com>
2024-08-24 17:08:53 +0200

    rpicam file added

---
+++ rpicam.py
@@ -0,0 +1,8 @@
+import os
+import main
+# Capture video from webcam
+image_number = 0
+cap = os.system("rpicam-still -o ./captures/" + image_number + "jpg")
+image_number += 1
+# Call the prediction function
+main.single_prediction(cap)
